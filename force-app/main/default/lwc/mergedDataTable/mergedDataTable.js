1d186dee9192e95c9e7075fb07c8e397ddcd58e5
Manjur <manjur2904@gmail.com>
2023-06-12 19:33:43 +0530

    Added spinner

--- force-app/main/default/lwc/mergedDataTable/mergedDataTable.js
+++ force-app/main/default/lwc/mergedDataTable/mergedDataTable.js
@@ -1,9 +1,10 @@
-import { LightningElement, track, wire } from 'lwc';
+import { LightningElement, track} from 'lwc';
 import getResultantList from '@salesforce/apex/UnifiedDataController.getResultantList';
 
 export default class MergedDataTable extends LightningElement {
     @track showData = false;
-    @track label = "Show Data";
+    @track label = "Show Merged Data";
+    @track isLoading;
     mergedTableColumns = [
         {label: 'BoM Part Number', fieldName: 'BoM_Port_Number__c'},
         {label: 'Quantity of BoM', fieldName: 'Bom_Quantity__c'},
@@ -14,18 +15,26 @@ export default class MergedDataTable extends LightningElement {
     
     mergedTableData;
 
-    handleShow(){
-        if(this.showData) this.label = "Show Data";
-        else this.label = "Hide Data";
-        
+    async handleShow(){
+        if(this.showData){
+            this.label = "Show Merged Data";
+            this.isLoading = false;
+        }
+        else{
+            this.label = "Hide Merged Data";
+            this.isLoading = true;
+            this.mergedTableData = [];
+        } 
+
         this.showData = !this.showData;
 
-        getResultantList()
+        await getResultantList()
         .then(result =>{
             this.mergedTableData = result;
         })
         .catch(error =>{
             this.mergedTableData = undefined;
         });
+        this.isLoading = false;
     }
 }
